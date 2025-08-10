95d8a0c151dfe0bdff73d08f2ef3e0a617540c2b
kannan-14-08-new <kannan14082003im@gmail.com>
2025-08-10 16:47:43 +0530

    add project

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -2,31 +2,39 @@ import project1_img from '../assets/amazon-app.png'
 import project2_img from '../assets/blog.png'
 import project3_img from '../assets/todo-list.png'
 import project4_img from '../assets/weather-app.png'
+import project5_img from '../assets/project-5.png'
 
 const mywork_data = [
     {
         w_no:1,
-        w_name:"amazon-app",
+        w_name:"Amazon-Clone-App",
         w_img:project1_img,
         href: "https://amazon-clone-project-liard.vercel.app/"
     },
     {
         w_no:2,
-        w_name:"blog",
+        w_name:"Blog",
         w_img:project2_img,
         href: "https://blog-website-iota-amber.vercel.app/"
     },
     {
         w_no:3,
-        w_name:"todo-list",
+        w_name:"Todo-List",
         w_img:project3_img,
         href: "https://todo-list-vert-six.vercel.app/"
     },
     {
         w_no:4,
-        w_name:"weather",
+        w_name:"Weather-App",
         w_img:project4_img,
         href: "https://weather-app-lime-ten-24.vercel.app/"
+    },
+
+     {
+        w_no:5,
+        w_name:"Animated Portfolio",
+        w_img:project5_img,
+        href: "https://animated-portfolio-rho-ten.vercel.app/"
     },
 ]
  
